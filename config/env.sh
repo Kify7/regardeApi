5b71d79d375d30042a3aa063b24746d1cfcfae11
Rachell Dguez <38174577+rachelldgz@users.noreply.github.com>
2021-09-24 15:26:43 -0500

    Delete env.sh

--- config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='development'
-export SECRET='secret' 
-export PORT=4001
-export DB_CONNECTION="mongodb+srv://admin:admin@cluster0.5x6ep.mongodb.net/regarde_bd?retryWrites=true&w=majority"
