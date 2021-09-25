58df1cac5a540a7687735146e49b1ba0a39cbebf
Rachel Domínguez <Rachy.Dgz@hotmail.com>
2021-09-25 13:37:49 -0500

    Get movies for name

--- Routes/movies.js
+++ Routes/movies.js
@@ -3,11 +3,18 @@ const {
     createMovie,
     getMovie,
     updateMovie,
-    deleteMovie
+    deleteMovie,
+    moviebyName
 } = require('../controllers/movies')
 const auth = require('./auth')
 
 router.post('/', auth.required, createMovie)
+
+router.get('/byName/:name', moviebyName)
+router.get('/byName', function (req, res) {
+    return res.sendStatus(404)
+})
+
 
 router.get('/:id', getMovie)
 router.get('/', getMovie)
