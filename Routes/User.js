4d5800d5c1d5d26b7a30bea2a1966b365293865e
Gibran Godoy <brian.godoy@alumnos.udg.mx>
2021-09-25 00:56:33 -0500

    Fixed errors on links

--- Routes/User.js
+++ Routes/User.js
@@ -10,13 +10,19 @@ const {
 } = require('../controllers/user')
 const auth=require('./auth')
 
-router.post('/', createUser)
 router.post('/login', logIn)
+router.post('/', createUser)
 router.get('/:id', auth.required, getUser)
 router.get('/', auth.required, getUser)
 router.put('/add-to-favorites/:id', auth.required, addToFavorites)
-router.put('/:id', auth.required, updateUser)
+router.put('/add-to-favorites', function (req, res) {
+    return res.sendStatus(404)
+})
 router.put('/remove-from-favorites/:id', auth.required, removeFromFavorites)
+router.put('/remove-from-favorites', function (req, res) {
+    return res.sendStatus(404)
+})
+router.put('/:id', auth.required, updateUser)
 router.delete('/:id', auth.required, deleteUser)
 
 module.exports = router;
--- Routes/comments.js
+++ Routes/comments.js
@@ -1,20 +1,26 @@
 const router = require('express').Router();
+// const { route } = require('.');
 const {
     createComment,
     getComment,
     updateComment,
     deleteComment,
-    commentsbyUsuario,
     commentsofMovie,
     commentsbyUser
 } = require('../controllers/comments')
 const auth = require('./auth')
 
 router.post('/', auth.required, createComment)
+router.get('/byuser/:id', commentsbyUser)
+router.get('/byuser', function (req, res) {
+    return res.sendStatus(404)
+})
+router.get('/ofmovie/:id', commentsofMovie)
+router.get('/ofmovie', function (req, res) {
+    return res.sendStatus(404)
+})
 router.get('/:id', getComment)
 router.get('/', getComment)
-router.get('/byuser/:id', commentsbyUser)
-router.get('/ofmovie/:id', commentsofMovie)
 router.put('/:id', auth.required, updateComment)
 router.delete('/:id', auth.required, deleteComment)
 
