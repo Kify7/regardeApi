5ed360b89f5716ac8f5dbcab758a9fd356844230
Gibran Godoy <brian.godoy@alumnos.udg.mx>
2021-09-25 12:49:40 -0500

    dotenv

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// require('dotenv').config();
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
