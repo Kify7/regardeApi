77c55f4628de5778248e4e317ea002d39da21cca
Gibran Godoy <brian.godoy@alumnos.udg.mx>
2021-09-25 12:18:47 -0500

    Changed to env.sh

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
+// require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
@@ -8,20 +8,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-//conecction db
+//connection db
 const mongoose = require('mongoose');
-console.log(process.env.DB_CONNECTION)
+
 mongoose.connect(process.env.DB_CONNECTION, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false,
         useCreateIndex: true
     })
-    .then(res => {
-        console.log('Connected to database ' + res.connections[0].name + ' ...');
-    }).catch(err => {
-        console.log('Error to connect to database!');
-    });
 
 mongoose.set("debug", true);
 
